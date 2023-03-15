752d87460050c1f2c1ef11dbd6e6165d6a45f82d
Md Sholayman <mdsholayman998@gmail.com>
2023-03-15 22:37:20 +0600

    code refactored

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ Description : A restful API to monitor up and down time of a user defined links
 */
 
 //dependencies
-
+const {handleReqRes} = require("./helpers/handleReqRes") ;
 const http = require("http") ;
-
-const url = require("url") ;
-
-const {StringDecoder} = require("string_decoder")
 
 
 // app object - module scaffolding
@@ -36,34 +32,7 @@ app.createServer = () =>{
 
 //handle Response
 
-app.handleReqRes = (req , res) =>{
- // request handle
- //get the url and parse it
- const parsedUrl = url.parse(req.url , true) ;
- const path = parsedUrl.pathname ;
- const trimmedPath = path.replace(/^\/+|\/+$/g, '') ;
- const method = req.method.toLowerCase() ;
- const queryStringObject = parsedUrl.query ;
- const headersObject = req.headers ;
-
-
-const decoder = new StringDecoder("utf-8") ;
-let realData = "" ;
-
-req.on("data" , (buffer) =>{
- realData += decoder.write(buffer) ;
-})
-
-req.on("end" , () =>{
-  realData += decoder.end() ;
-  console.log(realData);
-  //response handle
-  res.end("Hello World") ;
-})
-
-  
-}
-
+app.handleReqRes = handleReqRes ;
 //started the server
 
 app.createServer() ;
